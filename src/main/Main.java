b9341416ef1646a21d4045916981f11a04dd0568
marcosantonioprigolrossoni <marcosrossoni9@gmail.com>
2024-08-26 14:32:14 -0300

    primeiro commit

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
